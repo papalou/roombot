0103d9d0acbcf55dae7aedf1b2e44ebc13d1327e
LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>
2017-01-11 22:22:33 +0100

    update make_dev_env.sh to extract archive
    Signed-off-by: LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>

--- make_dev_env.sh
+++ make_dev_env.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-printf "Extract toolchain from archive\n"
+if [ -e toolchain ]; then
+	printf "rm existing toolchain folder\n"
+	rm -rf toolchain
+fi
 
-tar xvf archive/gcc-arm-none-eabi.tar.bz2 -C toolchain
+printf "Extract toolchain from archive\n"
+tar xvf archive/gcc-arm-none-eabi.tar.bz2 -C .
+
+mv gcc-arm-none-eabi-5_4-2016q3 toolchain
+
+
+if [ -e libopencm3 ]; then
+	printf "rm existing opencm3 folder\n"
+	rm -rf libopencm3
+fi
 
 printf "Extract libopencm3 from archive\n"
-
-tar xvf archive/libopencm3.tar -C libopencm3
+tar xvf archive/libopencm3.tar -C .
 
 exit 0
